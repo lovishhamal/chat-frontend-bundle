d3ca7952a1138585af1bd6456d237084a3ba0c5e
Lovish Hamal <lovishhamal@Lovishs-Laptop.local>
2023-02-12 20:19:05 +0545

    made upload file dynamic

--- src/components/chatComponent/chatBodyComponent.tsx
+++ src/components/chatComponent/chatBodyComponent.tsx
@@ -1,4 +1,5 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+import { SendOutlined, PlusOutlined, CloseOutlined } from "@ant-design/icons";
 import { ChatContext } from "../../context/chatContext";
 import { SET_MESSAGE } from "../../constants/actions";
 import { AuthContext } from "../../context";
@@ -8,10 +9,18 @@ import { VideoContext } from "../../context/videoContext";
 import AddUserListModal from "./sidebar/addUserListModal";
 import ChatBoxComponent from "./chatBoxComponent";
 import ChatBoxHeaderComponent from "./chatBoxHeaderComponent";
+import { UploadPhoto } from "../../common";
+import { Divider } from "antd";
+import Styles from "./chatBodyComponent.module.css";
+import { getCurrentDate } from "../../util/date";
 
 export const ChatBodyComponent = () => {
+  const inputRef = useRef<any>(null);
+  const imageRef = useRef<any>(null);
+
   const messagesEndRef = useRef<any>(null);
   const modalRef = useRef<any>(null);
+  const [showUploadFile, setShowUploadFile] = useState<boolean>(false);
 
   const { socket } = useContext<any>(VideoContext);
   const { state, dispatch } = useContext<any>(ChatContext);
@@ -46,15 +55,87 @@ export const ChatBodyComponent = () => {
       messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     }
   }, [messagesEndRef.current, state]);
+  const onClickSend = () => {
+    let message: any = {
+      connectionId: state?.user.connectionId,
+      sentBy: authState.user?._id,
+      sentTo: state.user._id,
+      text: inputRef.current.value,
+      updatedAt: getCurrentDate(),
+    };
+    if (imageRef.current) {
+      const image = {
+        name: imageRef.current.name,
+        size: imageRef.current.size,
+        data: imageRef.current.thumbUrl,
+        type: imageRef.current.type,
+      };
+      message = { ...message, image };
+    }
+
+    const body = {
+      messageId: state?.user.messageId,
+      createdAt: getCurrentDate(),
+      connectionId: state?.user.connectionId,
+      message,
+    };
+    socket.emit("send_message", body);
+    inputRef.current.value = "";
+  };
 
   return (
     <>
       <CustomModal ref={modalRef} title="Create a group" footer={false}>
         <AddUserListModal closeModal={closeModal} />
       </CustomModal>
-      <div style={{ width: "100%" }}>
-        <ChatBoxHeaderComponent modalRef={modalRef} />
-        <ChatBoxComponent />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          style={{
+            justifySelf: "flex-start",
+            height: "100%",
+            overflow: "scroll",
+          }}
+        >
+          <ChatBoxHeaderComponent modalRef={modalRef} />
+          <ChatBoxComponent />
+        </div>
+        <div className={Styles.messageInputWrapper}>
+          {showUploadFile && (
+            <div>
+              <UploadPhoto
+                handleChange={(item: any) => (imageRef.current = item)}
+              />
+              <Divider />
+            </div>
+          )}
+          <div className={Styles.sendNewMessage}>
+            <button
+              className="addFiles"
+              onClick={() => setShowUploadFile(!showUploadFile)}
+            >
+              {showUploadFile ? <CloseOutlined /> : <PlusOutlined />}
+            </button>
+            <input
+              type="text"
+              placeholder="Type a message here"
+              ref={inputRef}
+            />
+            <button
+              className="btnSendMsg"
+              id="sendMsgBtn"
+              onClick={onClickSend}
+            >
+              <SendOutlined />
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/chatComponent/chatBoxComponent.tsx
+++ src/components/chatComponent/chatBoxComponent.tsx
@@ -1,61 +1,22 @@
 import { useContext, useRef, useState } from "react";
-import { Divider, List } from "antd";
+import { List } from "antd";
 import { SenderBoxComponent } from "./senderBoxComponent";
 import { ReceiverBoxComponent } from "./receiverBoxComponent";
 import { IMessage } from "../../interface/components/chat/chatInterface";
 import Styles from "./chatBodyComponent.module.css";
-import { SendOutlined, PlusOutlined, CloseOutlined } from "@ant-design/icons";
 import { AuthContext, ChatContext } from "../../context";
 import { VideoContext } from "../../context/videoContext";
 import { getCurrentDate } from "../../util/date";
-import { UploadPhoto } from "../../common";
 
 const ChatBoxComponent = () => {
-  const { socket } = useContext<any>(VideoContext);
-  const [showUploadFile, setShowUploadFile] = useState<boolean>(false);
   const { state } = useContext<any>(ChatContext);
   const { state: authState } = useContext<any>(AuthContext);
-  const inputRef = useRef<any>(null);
-  const imageRef = useRef<any>(null);
 
   const messagesEndRef = useRef<any>(null);
-  const modalRef = useRef<any>(null);
-
-  const onClickSend = () => {
-    let message: any = {
-      connectionId: state?.user.connectionId,
-      sentBy: authState.user?._id,
-      sentTo: state.user._id,
-      text: inputRef.current.value,
-      updatedAt: getCurrentDate(),
-    };
-    if (imageRef.current) {
-      const image = {
-        name: imageRef.current.name,
-        size: imageRef.current.size,
-        data: imageRef.current.thumbUrl,
-        type: imageRef.current.type,
-      };
-      message = { ...message, image };
-    }
-
-    const body = {
-      messageId: state?.user.messageId,
-      createdAt: getCurrentDate(),
-      connectionId: state?.user.connectionId,
-      message,
-    };
-    socket.emit("send_message", body);
-    inputRef.current.value = "";
-  };
 
   return (
     <div className={Styles.mainChatContent}>
-      <div
-        className={
-          showUploadFile ? Styles.contentBody : Styles.contentBodyOverflow
-        }
-      >
+      <div>
         <List
           style={{ width: "100%", overflow: "scroll" }}
           itemLayout="horizontal"
@@ -73,28 +34,6 @@ const ChatBoxComponent = () => {
             );
           }}
         />
-      </div>
-      <div className={Styles.messageInputWrapper}>
-        {showUploadFile && (
-          <div>
-            <UploadPhoto
-              handleChange={(item: any) => (imageRef.current = item)}
-            />
-            <Divider />
-          </div>
-        )}
-        <div className={Styles.sendNewMessage}>
-          <button
-            className="addFiles"
-            onClick={() => setShowUploadFile(!showUploadFile)}
-          >
-            {showUploadFile ? <CloseOutlined /> : <PlusOutlined />}
-          </button>
-          <input type="text" placeholder="Type a message here" ref={inputRef} />
-          <button className="btnSendMsg" id="sendMsgBtn" onClick={onClickSend}>
-            <SendOutlined />
-          </button>
-        </div>
       </div>
     </div>
   );
